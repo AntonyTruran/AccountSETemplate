d1ebbd358b1e12deae34d770a9d492bfdad7a77d
tnj96 <talesha267@gmail.com>
2019-02-13 09:41:17 +0000

    Entity Manager Added

--- src/main/java/com/qa/persistence/domain/Account.java
+++ src/main/java/com/qa/persistence/domain/Account.java
@@ -1,10 +1,24 @@
 package com.qa.persistence.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Account {
 
+	@Id @GeneratedValue(strategy=GenerationType.AUTO)
 	private Long id;
+	
+	@Column(length = 255)
 	private String firstName;
+	
+	@Column(length = 255)
 	private String lastName;
+	
+	@Column(length = 10)
 	private String accountNumber;
 
 	public Account(Long id, String firstName, String lastName, String accountNumber) {
